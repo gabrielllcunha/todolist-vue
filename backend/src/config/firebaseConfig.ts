b7319f3967e2be5ee17428490fe859d3f3b5e41b
gabrielllcunha <gabrielcunhasantos01@gmail.com>
2024-10-08 13:59:32 -0300

    fix: typo error

--- backend/src/config/firebaseConfig.ts
+++ backend/src/config/firebaseConfig.ts
@@ -3,7 +3,7 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APP_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,

3b9fa4dbb299bb15d370da766a90bce088063b83
gabrielllcunha <gabrielcunhasantos01@gmail.com>
2024-10-15 10:47:52 -0300

    chore: deleta pasta backend

--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { server } from "./config/server";
-
-import dotenv from "dotenv";
-
-dotenv.config();
-
-server.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
-});
